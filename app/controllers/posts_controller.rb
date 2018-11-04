839523cbb79db14f1992fc4be2f6571949109346
ws903 <ws903@nyu.edu>
2018-11-03 20:49:55 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,8 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  def create
+    @post = Post.create(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
+  end
 end
